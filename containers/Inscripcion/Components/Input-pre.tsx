5eb29e2093a9e45314c0c38ed03da64126c83b1d
mauryo <mauriciomorrison@hotmail.com>
2022-04-10 15:18:32 -0500

    se agrega seccion de inscripciones

---
+++ containers/Inscripcion/Components/Input-pre.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable max-len */
+// ---Dependencys
+import { ReactElement } from 'react';
+import { Col, Row, Button } from 'antd';
+// ---Components
+
+// ----------------------------------------COMPONENT----------------------------------------
+export default function InputPre(): ReactElement {
+  return (
+    <Row>
+      <Col xs={24} sm={24} md={24} lg={18} xl={18}>
+        <input type="text" placeholder="Nombre" />
+        <input type="text" placeholder="Celular" />
+        <input type="text" placeholder="Correo Electronico" />
+      </Col>
+      <Col xs={24} sm={24} md={24} lg={6} xl={6}>
+        <Button>Quiero Informacion</Button>
+      </Col>
+    </Row>
+  );
+}
